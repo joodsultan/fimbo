87f1c233195db31e2bb3dce23a84eb5ca6d80d1d
joodsultan <66631701+joodsultan@users.noreply.github.com>
2020-10-26 20:22:56 +0300

    .

--- projectforensic/particles.js
+++ projectforensic/particles.js
@@ -1,17 +1,8 @@
-/* -----------------------------------------------
-/* Author : Vincent Garreau  - vincentgarreau.com
-/* MIT license: http://opensource.org/licenses/MIT
-/* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
-/* How to use? : Check the GitHub README
-/* v2.0.0
-/* ----------------------------------------------- */
 
 var pJS = function(tag_id, params){
 
   var canvas_el = document.querySelector('#'+tag_id+' > .particles-js-canvas-el');
 
-  /* particles.js variables with default values */
   this.pJS = {
     canvas: {
       el: canvas_el,
@@ -136,7 +127,6 @@ var pJS = function(tag_id, params){
 
   var pJS = this.pJS;
 
-  /* params settings */
   if(params){
     Object.deepExtend(pJS, params);
   }
